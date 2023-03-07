533791938178eaf90d9c9dd4fc4e0957a2285438
muhammadderic <125786806+muhammadderic@users.noreply.github.com>
2023-03-07 14:41:17 +0700

    Create recoil's state for storing the input

--- src/components/InputTugas.js
+++ src/components/InputTugas.js
@@ -1,12 +1,36 @@
 // React
-import React from 'react';
+import React, { useState } from 'react';
+// Recoil
+import { useSetRecoilState } from 'recoil';
+import { daftarTugasState } from '../recoil_state';
 // MUI
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
 
+// Generate unique ID
+let id = 0;
+function getId() {
+  return id++;
+}
+
 export default function InputTugas() {
+  const [inputValue, setInputValue] = useState('');
+  const setDaftarTugas = useSetRecoilState(daftarTugasState);
+
+  const addTugas = () => {
+    setDaftarTugas((oldDaftarTugas) => [
+      ...oldDaftarTugas,
+      {
+        id: getId(),
+        description: inputValue,
+        isCompleted: false,
+      },
+    ]);
+    setInputValue('');
+  };
+
   return (
     <Container sx={{ mt: 3 }}>
       <Box
@@ -15,8 +39,16 @@ export default function InputTugas() {
         autoComplete="off"
         sx={{ display: 'flex' }}
       >
-        <TextField label="Deskripsi" fullWidth variant="outlined" />
-        <Button variant="contained">Add</Button>
+        <TextField
+          label="Deskripsi"
+          variant="outlined"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          fullWidth
+        />
+        <Button variant="contained" onClick={addTugas}>
+          Add
+        </Button>
       </Box>
     </Container>
   );
