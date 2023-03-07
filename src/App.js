4d8a6df152b5f15b19fe1ca7f0600736ffd24c84
muhammadderic <125786806+muhammadderic@users.noreply.github.com>
2023-03-07 13:07:08 +0700

    Create homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+// React
 import React from 'react';
+// MUI
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello Deric</h1>
-    </div>
+    <Container maxWidth="md" sx={{ display: 'flex', justifyContent: 'center' }}>
+      <Box
+        sx={{
+          width: '75%',
+          p: 3,
+          borderRadius: 3,
+          boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px;',
+        }}
+      >
+        <Typography
+          variant="h3"
+          align="center"
+          color="primary"
+          gutterBottom
+          sx={{ fontWeight: 'bold' }}
+        >
+          Daftar Tugas
+        </Typography>
+      </Box>
+    </Container>
   );
 }
